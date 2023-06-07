edd440557271fcfae757e4b8e5cc64458269a39f
Kristian Numme <k.numme05@gmail.com>
2023-06-07 12:31:10 +0200

    player icon

--- gameScripts/player.js
+++ gameScripts/player.js
@@ -7,14 +7,14 @@ export default class Player {
     constructor(positionX, positionY, icon, width = 50, height = 100) {
         this.#positionX = positionX;
         this.#positionY = positionY;
-        this.icon = icon;
+        this.icon = "./Images/Ikoner/player.png";
         this.#width = width;
         this.#height = height;
         this.#addToDOM();
     };
     #addToDOM() {
         // Create DOM element
-        this.#playerElement = document.createElement('div');
+        this.#playerElement = document.createElement('img');
 
         // Apply CSS styling
         this.#playerElement.style.position = "absolute";
@@ -23,6 +23,7 @@ export default class Player {
         this.#playerElement.style.width = `${this.#width}px`;
         this.#playerElement.style.height = `${this.#height}px`;
         this.#playerElement.style.background = this.icon || "blue";
+        this.#playerElement.src = this.icon;
 
         // Append the div to the player area
         document.getElementById("player-area").appendChild(this.#playerElement);
