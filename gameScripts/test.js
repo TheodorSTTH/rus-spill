7f85adbe197698da4a4b52de032f102f082b172a
Sir-Nico <rennemo@protonmail.com>
2023-06-07 11:39:21 +0200

    goofy ahh player movement

--- gameScripts/test.js
+++ gameScripts/test.js
@@ -1,50 +1,24 @@
-import Alcohol from "./alcohol.js";
 import Coke from "./coke.js";
-import Bear from "./bear.js";
 import EffectManager from "./EffectManager.js";
-import Fleinsopp from "./fleinsopp.js";
 import Hasj from "./hasj.js";
 import SceneManager from "./sceneManager.js";
 import Stats from "./stats.js";
 import Player from "./player.js";
-import Utils from "./utils.js";
 
 const mySceneManager = new SceneManager(1, 0);
 const myStats = new Stats();
 const player = new Player(800, 100);
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max)) + 1;
-}
-function randomItem() {
-    let newItem;
-    const randomInt = Utils.getRandomInt(4);
-    console.log(randomInt)
-    switch (randomInt) {
-        case 1:
-            newItem = new Hasj(0, 0);
-            break;
-        case 2:
-            newItem = new Alcohol(0, 0);
-            break;
-        case 3:
-            newItem = new Coke(0, 0);
-            break;
-        case 4:
-            newItem = new Fleinsopp(0, 0);
-            break;
-        case 5:
-            newItem = new Bear(0, 0);
-            break;
-        default:
-            newItem = new Alcohol(0, 0);
-            break;
-    }
-    newItem.positionY = Math.random() * (500 - newItem.height)
-    return newItem
-}
+function makeRandomItem() {
+    const newHasj = new Coke(0, 0);
+    newHasj.positionY = Math.random() * (500 - newHasj.height)
+    const newHasj2 = new Hasj(0, 0);
+    newHasj2.positionY = Math.random() * (500 - newHasj2.height)
 
-let directionY = -1;
+    mySceneManager.enqueue(newHasj);
+    mySceneManager.enqueue(newHasj2);
+}
+let directionY = -1
 document.addEventListener('keydown', function(event) {
     if (event.code === 'Space' || event.keyCode === 32) {
         directionY = 1;
@@ -56,22 +30,16 @@ document.addEventListener('keyup', function(event) {
     };
 });
 
-function makeRandomItem() {
-    const newItem = randomItem();
-    mySceneManager.enqueue(newItem);
-}
-
 let spawnrateCount = 0;
 function update() { // TODO: better update function (Vsync & deltaTime)
-    if (myStats.health === 0) Utils.replaceUrl('game.html', 'Endscreen.html');
     spawnrateCount++;
+    mySceneManager.step();
     player.positionY += 7 * directionY;
     if (player.positionY <=0) {
         player.positionY = 0;
     } else if (player.positionY >= 400) {
         player.positionY = 400;
     }
-    mySceneManager.step();
     // ! NICO om du plotter inn spiller data under så burde kollisjoner med drugs fungere
     const collidingItem = mySceneManager.getCollidingItem(player.positionX, player.positionY, player.width, player.height);
     if (collidingItem) {
@@ -82,7 +50,6 @@ function update() { // TODO: better update function (Vsync & deltaTime)
         makeRandomItem();
         spawnrateCount = 0;
     }
-
 }
 
 const frameRate = 60; // Set the desired frame rate
